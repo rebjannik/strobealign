rebjannik/strobealign

commit 91f22234baf192a3167354a7f2bbfefb49cb2162

Split merge_hits_into_nams() into two functions

src/nam.cpp
@@ -9,7 +9,7 @@ struct Hit {
     int ref_end;
 };
 
-void add_to_hits_per_ref(
+inline void add_to_hits_per_ref(
     robin_hood::unordered_map<unsigned int, std::vector<Hit>>& hits_per_ref,
     int query_start,
     int query_end,
@@ -28,18 +28,15 @@ void add_to_hits_per_ref(
     }
 }
 
-std::vector<Nam> merge_hits_into_nams(
-    std::array<robin_hood::unordered_map<unsigned int, std::vector<Hit>>, 2>& hits_per_ref,
+void merge_hits_into_nams(
+    robin_hood::unordered_map<unsigned int, std::vector<Hit>>& hits_per_ref,
     int k,
-    bool sort
+    bool sort,
+    bool is_revcomp,
+    std::vector<Nam>& nams  // inout
 ) {
-    std::vector<Nam> nams;
     int nam_id_cnt = 0;
-
-    for (size_t is_revcomp = 0; is_revcomp < 2; ++is_revcomp) {
-        auto& hits_oriented = hits_per_ref[is_revcomp];
-
-    for (auto &[ref_id, hits] : hits_oriented) {
+    for (auto &[ref_id, hits] : hits_per_ref) {
         if (sort) {
             std::sort(hits.begin(), hits.end(), [](const Hit& a, const Hit& b) -> bool {
                     // first sort on query starts, then on reference starts
@@ -135,7 +132,18 @@ std::vector<Nam> merge_hits_into_nams(
             n.score = n_score;
             nams.push_back(n);
         }
-        }
+    }
+}
+
+std::vector<Nam> merge_hits_into_nams_forward_and_reverse(
+    std::array<robin_hood::unordered_map<unsigned int, std::vector<Hit>>, 2>& hits_per_ref,
+    int k,
+    bool sort
+) {
+    std::vector<Nam> nams;
+    for (size_t is_revcomp = 0; is_revcomp < 2; ++is_revcomp) {
+        auto& hits_oriented = hits_per_ref[is_revcomp];
+        merge_hits_into_nams(hits_oriented, k, sort, is_revcomp, nams);
     }
     return nams;
 }
@@ -168,7 +176,7 @@ std::pair<float, std::vector<Nam>> find_nams(
         }
     }
     float nonrepetitive_fraction = total_hits > 0 ? ((float) nr_good_hits) / ((float) total_hits) : 1.0;
-    auto nams = merge_hits_into_nams(hits_per_ref, index.k(), false);
+    auto nams = merge_hits_into_nams_forward_and_reverse(hits_per_ref, index.k(), false);
     return make_pair(nonrepetitive_fraction, nams);
 }
 
@@ -229,7 +237,7 @@ std::vector<Nam> find_nams_rescue(
         }
     }
 
-    return merge_hits_into_nams(hits_per_ref, index.k(), true);
+    return merge_hits_into_nams_forward_and_reverse(hits_per_ref, index.k(), true);
 }
 
 std::ostream& operator<<(std::ostream& os, const Nam& n) {

commit 8faed420928ef2ee48422c2083034129edcf73cf

Fix: Avoid nam id collisions that reduce accuracy
This fixes a regression introduced by commit 91f2223 and brings accuracy to
the expected levels (higher than v0.11.0 due to tuned parameters).

See #345

src/nam.cpp
@@ -35,7 +35,6 @@ void merge_hits_into_nams(
     bool is_revcomp,
     std::vector<Nam>& nams  // inout
 ) {
-    int nam_id_cnt = 0;
     for (auto &[ref_id, hits] : hits_per_ref) {
         if (sort) {
             std::sort(hits.begin(), hits.end(), [](const Hit& a, const Hit& b) -> bool {
@@ -81,8 +80,6 @@ void merge_hits_into_nams(
             // Add the hit to open matches
             if (!is_added){
                 Nam n;
-                n.nam_id = nam_id_cnt;
-                nam_id_cnt ++;
                 n.query_start = h.query_start;
                 n.query_end = h.query_end;
                 n.ref_start = h.ref_start;
@@ -110,6 +107,7 @@ void merge_hits_into_nams(
                         n_score = ( 2*n_min_span -  n_max_span) > 0 ? (float) (n.n_hits * ( 2*n_min_span -  n_max_span) ) : 1;   // this is really just n_hits * ( min_span - (offset_in_span) ) );
 //                        n_score = n.n_hits * n.query_span();
                         n.score = n_score;
+                        n.nam_id = nams.size();
                         nams.push_back(n);
                     }
                 }
@@ -130,6 +128,7 @@ void merge_hits_into_nams(
             n_score = ( 2*n_min_span -  n_max_span) > 0 ? (float) (n.n_hits * ( 2*n_min_span -  n_max_span) ) : 1;   // this is really just n_hits * ( min_span - (offset_in_span) ) );
 //            n_score = n.n_hits * n.query_span();
             n.score = n_score;
+            n.nam_id = nams.size();
             nams.push_back(n);
         }
     }