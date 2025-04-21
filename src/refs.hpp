93002e43fb3be6570390531c5b653f8d364650ba
rebjannik <becca@Molly>
2025-04-21 22:03:30 +0200

    conversion implemented

--- src/refs.hpp
+++ src/refs.hpp
@@ -7,6 +7,24 @@
 #include <numeric>
 #include <vector>
 #include "exceptions.hpp"
+static unsigned char seq_nt4_table[256] = {
+    0, 1, 2, 3,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  3, 3, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  3, 3, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4
+};
 
 class References {
     typedef std::vector<unsigned int> ref_lengths;
@@ -27,10 +45,19 @@ public:
         // Here is the conversion function for the byte size
         // Each vector is a byte (or 4 characters), 
         lengths.reserve(sequences.size());
+
         for (auto& seq : sequences) {
             lengths.push_back(seq.size());
         }
+
         _total_length = std::accumulate(this->lengths.begin(), this->lengths.end(), (size_t)0);
+        
+        for ( int i = 0; i < _total_length; ++i){
+            for (char nucleotide : sequences[i]) {
+                int c = seq_nt4_table[(uint8_t)nucleotide];
+                sequences[i] = c < 4 ? nucleotide : 3;
+            }
+        }
     }
 
     void add(std::string&& name, std::vector<int>&& sequence);
@@ -44,6 +71,7 @@ public:
     size_t total_length() const {
         return _total_length;
     }
+
 
     std::vector<std :: vector<int>> sequences;
     ref_names names;
